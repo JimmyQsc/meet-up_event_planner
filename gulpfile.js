246ec9becb2baeb89fe87b776a63ca4cf67b7186
Jimmy Q <marioqsc2013@hotmail.com>
2016-08-15 17:21:07 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+/*eslint-env node */
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
+
+
+
+
+
+gulp.task('default', ['styles', 'lint'], function() {
+    gulp.watch('scss/**/*.scss', ['styles']);
+
+    browserSync.init({
+        server: './'
+    });
+
+    browserSync.stream();
+});
+
+gulp.task('lint', function(){
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('styles', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(''));
+});
